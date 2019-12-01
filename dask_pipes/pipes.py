36ac1bdbd38c8efd573b2675044f8ac1fdc27755
Artyom Fomenko <a.fomenko@mafin.ru>
2019-12-01 16:21:48 +0300

    Minor fix'

--- dask_pipes/pipes.py
+++ dask_pipes/pipes.py
@@ -1,4 +1,5 @@
-import dask_pipes as dp
+from .base.pipeline import Pipeline
+from .nodes import AddNaIndicator, RobustCategoriser, DateProcessor, as_node, AddNaCategory
 import dask_ml.preprocessing
 import dask_ml.compose
 import dask_ml.impute
@@ -8,31 +9,31 @@ __all__ = ['prepareNN']
 
 
 def prepareNN():
-    pipeline = dp.Pipeline()
+    pipeline = Pipeline()
 
-    categorize = dp.RobustCategoriser()
-    convert_dates = dp.DateProcessor(retro_date_mapping={'c5': 'c7'})
+    categorize = RobustCategoriser()
+    convert_dates = DateProcessor(retro_date_mapping={'c5': 'c7'})
 
-    add_nullable_indicator = dp.as_node("add_na_indicator", dask_ml.compose.ColumnTransformer([
-        ('add_na_indicator', dp.AddNaIndicator(), cs.Numeric & cs.Nullable)
+    add_nullable_indicator = as_node("add_na_indicator", dask_ml.compose.ColumnTransformer([
+        ('add_na_indicator', AddNaIndicator(), cs.Numeric & cs.Nullable)
     ], remainder='passthrough'))
 
-    add_na_cat = dp.AddNaCategory()
+    add_na_cat = AddNaCategory()
 
     # FILLNA TRANSFORMER
-    fillna = dp.as_node('fillna', dask_ml.compose.ColumnTransformer([
+    fillna = as_node('fillna', dask_ml.compose.ColumnTransformer([
         ('fillna_numeric', dask_ml.impute.SimpleImputer(strategy='mean'), cs.Numeric),
         ('fillna_str', dask_ml.impute.SimpleImputer(strategy='constant', fill_value='<Unknown>'), cs.Categorical)
     ], remainder='passthrough'))
 
     # STANDARD SCALER
-    scale = dp.as_node('scale', dask_ml.compose.ColumnTransformer([
+    scale = as_node('scale', dask_ml.compose.ColumnTransformer([
         ('scale_numeric', dask_ml.preprocessing.StandardScaler(), cs.Numeric)
     ], remainder='passthrough'))
 
-    one_hot = dp.as_node('one_hot', dask_ml.preprocessing.DummyEncoder(drop_first=True))
+    one_hot = as_node('one_hot', dask_ml.preprocessing.DummyEncoder(drop_first=True))
 
-    scale_one_hot = dp.as_node('scale_one_hot', dask_ml.compose.ColumnTransformer([
+    scale_one_hot = as_node('scale_one_hot', dask_ml.compose.ColumnTransformer([
         ('scale_one_hot', dask_ml.preprocessing.MinMaxScaler(feature_range=(-1, 1)), cs.Numeric & cs.Binary)
     ], remainder='passthrough'))
 
